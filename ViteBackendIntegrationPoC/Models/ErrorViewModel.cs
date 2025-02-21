897b613127ccc2c58adedee2f50999c1c9dee1bf
Carsten Lindner <Carsten.Lindner@flaschenpost.de>
2025-02-21 11:53:50 +0100

    Add project files.

---
+++ ViteBackendIntegrationPoC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViteBackendIntegrationPoC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
