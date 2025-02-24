112ed64815a3bbafc695a0663c17deff532ecb07
Carsten Lindner <Carsten.Lindner@flaschenpost.de>
2025-02-24 22:21:23 +0100

    Refactor HomeController and views for improved structure; remove Privacy page and update Error handling

--- ViteBackendIntegrationPoC/Models/ErrorViewModel.cs
+++ ViteBackendIntegrationPoC/Models/ErrorViewModel.cs
@@ -3,7 +3,5 @@ namespace ViteBackendIntegrationPoC.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
